4bd1281c766ec1d7df5396c222d8cc339567dbaf
Mylie Walker <myliewalker@Mylies-MacBook-Pro.local>
2018-09-15 14:40:58 -0400

    read

--- src/NBody.java
+++ src/NBody.java
@@ -38,14 +38,10 @@ public class NBody {
 			int nb = s.nextInt();
 			double rad = s.nextDouble();
 			Body[] bodies = new Body[nb];
-			// for(int k=0; k < nb; k++) {
-			// 	bodies[k] = new Body(s.nextDouble(), s.nextDouble(), s.nextDouble(), s.nextDouble(), s.nextDouble(), s.next());
-			// }
-			// s.close();
-			// return bodies;
 			for (int i = 0; i < nb; i++)
 			{
-				bodies[i] = new Body.Body(s.nextDouble(), s.nextDouble(), s.nextDouble(), s.nextDouble(), s.nextDouble(), s.next());
+				// bodies[i] = new Body(s.nextDouble(), s.nextDouble(), s.nextDouble(), s.nextDouble(), s.nextDouble(), s.next());
+				bodies[i] = new Body(s.getX(), s.getY(), s.getXVel(), s.getYVel(), s.getMass(), s.getName());
 			}
 			return bodies;
 	}
